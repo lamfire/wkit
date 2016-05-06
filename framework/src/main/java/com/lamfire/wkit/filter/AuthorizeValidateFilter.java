0e0c230aa9a3c5551e82a6e58c91cb2e78f512a3
lamfire <52089489@qq.com>
2016-05-06 13:35:43 +0800

    add exclude setting

--- framework/src/main/java/com/lamfire/wkit/filter/AuthorizeValidateFilter.java
+++ framework/src/main/java/com/lamfire/wkit/filter/AuthorizeValidateFilter.java
@@ -1,6 +1,8 @@
 package com.lamfire.wkit.filter;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,8 +20,16 @@ import com.lamfire.utils.StringUtils;
 
 public class AuthorizeValidateFilter implements Filter{
 	static final Logger LOGGER = Logger.getLogger(AuthorizeValidateFilter.class);
+    private static final String AUTH_SESSION_KEY = "auth.session.key";
+    private static final String AUTH_REDIRECT_URL = "auth.redirect.url";
+    private static final String AUTH_EXCLUDE_PATHS = "auth.exclude.paths";
+    private static final String AUTH_EXCLUDE_REGEX = "auth.exclude.regex";
+
+    private final Set<String> excludePaths = new HashSet<String>();
+    private String excludeRegex;
 	private String sessionKey;
 	private String redirectUrl;
+
 	
 	public void destroy() {
 		
@@ -32,21 +42,47 @@ public class AuthorizeValidateFilter implements Filter{
 		return matcher.find();
 	}
 
+    boolean isExcludeServletPath(String servletPath){
+        for(String path : excludePaths){
+            if(StringUtils.isStartWith(servletPath,path)){
+                return true;
+            }
+        }
+
+        if(StringUtils.isBlank(excludeRegex)){
+            return false;
+        }
+
+        Pattern pattern = Pattern.compile(excludeRegex);
+        Matcher matcher = pattern.matcher(servletPath);
+        return matcher.find();
+    }
+
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse res = (HttpServletResponse)response;
 
-        String uri = req.getRequestURI();
-        if(StringUtils.contains(uri,redirectUrl)){
+        //ignore redirect url
+        String servletPath = req.getServletPath();
+        if(StringUtils.equalsIgnoreCase(servletPath,redirectUrl)){
             chain.doFilter(request, response);
             return;
         }
 
+        //ignore exclude paths
+        if(isExcludeServletPath(servletPath)){
+            chain.doFilter(request, response);
+            return;
+        }
+
+
+        //auth session
 		if(req.getSession().getAttribute(sessionKey) != null){
 			chain.doFilter(request, response);
 			return;
 		}
-		
+
+        //false
 		if(isExternalUrl(redirectUrl)){
 			res.sendRedirect(redirectUrl);
 			return;
@@ -56,10 +92,22 @@ public class AuthorizeValidateFilter implements Filter{
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
-		this.sessionKey = filterConfig.getInitParameter("auth.session.key");
+		this.sessionKey = filterConfig.getInitParameter(AUTH_SESSION_KEY);
 		LOGGER.info("auth session key : " + this.sessionKey);
 		
-		this.redirectUrl = filterConfig.getInitParameter("auth.redirect.url");
+		this.redirectUrl = filterConfig.getInitParameter(AUTH_REDIRECT_URL);
 		LOGGER.info("auth redirect url : " + this.redirectUrl);
+
+        String excludes =  filterConfig.getInitParameter(AUTH_EXCLUDE_PATHS);
+        LOGGER.info("auth exclude paths : " + excludes);
+        if(StringUtils.isNotBlank(excludes)){
+            String [] paths  = StringUtils.split(excludes,',');
+            for(String path : paths){
+                this.excludePaths.add(path);
+            }
+        }
+
+        this.excludeRegex =  filterConfig.getInitParameter(AUTH_EXCLUDE_REGEX);
+        LOGGER.info("auth exclude regex : " + excludeRegex);
 	}
 }
