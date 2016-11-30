dd80056aeab83f0eba7fe634d64271170ae6c8a0
lamfire <52089489@qq.com>
2016-11-30 14:15:53 +0800

    version to 2.0.4

--- framework/src/main/java/com/lamfire/wkit/Version.java
+++ framework/src/main/java/com/lamfire/wkit/Version.java
@@ -6,7 +6,7 @@ import com.lamfire.utils.StringUtils;
 
 public final class Version {
 	private static final Logger LOGGER = Logger.getLogger(Version.class);
-	public static final String VERSION = "2.0.3";
+	public static final String VERSION = "2.0.4";
 
 	static{
 		JvmInfo jvm = JvmInfo.getInstance();
